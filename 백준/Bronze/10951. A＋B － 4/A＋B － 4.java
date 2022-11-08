d3f0cff54df008f43be6e6e54f044e35764d664b
김수경 <107246798+ksklaura@users.noreply.github.com>
2022-11-08 22:49:57 +0900

    Update A＋B － 4.java

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -19,6 +19,7 @@ public class Main {
 		int b;
 		
 		while((str = br.readLine()) != null) { 		// str 문자열 변수에 입력한 문자를 할당하여 null인지 확인
+								// readLine() 을 통해 입력을 하여 str 에 저장된 데이터가 null 일 경우 while 반복문을 종료시켜버리고 아닐 경우 반복문을 계속 수행
 			st = new StringTokenizer(str, " "); 	// null이 아니라면 해당 문자열을 " "로 구분
 			
 			a = Integer.parseInt(st.nextToken());
